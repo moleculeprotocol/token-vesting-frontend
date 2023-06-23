3731c0d166b41f457b0dfba9e555d9450fde0abe
Schmackofant <schmackofant@protonmail.com>
2023-06-23 11:51:32 +0200

    test

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig, loadEnv } from "@wagmi/cli"
+import { defineConfig /* loadEnv */ } from "@wagmi/cli"
 import { actions, react } from "@wagmi/cli/plugins"
 
 import { abi } from "@/lib/vestingAbi"
 
 export default defineConfig(() => {
-  const env = loadEnv({
-    mode: process.env.NODE_ENV,
-    envDir: process.cwd(),
-  })
+  // const env = loadEnv({
+  //   mode: process.env.NODE_ENV,
+  //   envDir: process.cwd(),
+  // })
   return {
     out: "wagmi/generated.ts",
     contracts: [
@@ -15,8 +15,7 @@ export default defineConfig(() => {
         name: "TokenVesting",
         abi: abi,
         address: {
-          [env.NEXT_PUBLIC_CHAIN_ID]:
-            env.NEXT_PUBLIC_VESTING_CONTRACT as `0x${string}`,
+          1: "0x4f55eDFfd4e9325577D4C8Dde6F15782CEfd6517" as `0x${string}`,
         },
       },
     ],
